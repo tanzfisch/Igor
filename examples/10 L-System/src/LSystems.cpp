65643c4aad05df187fefbd3a11015dd30ae1cd89
tanzfisch <martinloga@protonmail.com>
2016-09-19 12:19:12 +0200

    started new example for developing L-Systems

---
+++ examples/10 L-System/src/LSystems.cpp
@@ -0,0 +1,236 @@
+#include "LSystems.h"
+
+#include <iMaterial.h>
+#include <iMaterialGroup.h>
+#include <iNodeVisitorPrintTree.h>
+#include <iTaskManager.h>
+#include <iNodeCamera.h>
+#include <iNodeModel.h> 
+#include <iNodeTransform.h>
+#include <iRenderer.h>
+#include <iApplication.h>
+#include <iSceneFactory.h>
+#include <iScene.h>
+#include <iNodeFactory.h>
+#include <iMouse.h>
+#include <iKeyboard.h>
+#include <iTimer.h>
+#include <iTextureFont.h>
+#include <iNodeLight.h>
+#include <iModelResourceFactory.h>
+#include <iTaskFlushModels.h>
+#include <iTaskFlushTextures.h>
+#include <iMaterialResourceFactory.h>
+#include <iStatistics.h>
+#include <iNodeSwitch.h>
+#include <iNodeLODSwitch.h>
+#include <iNodeLODTrigger.h>
+#include <iGradient.h>
+using namespace Igor;
+
+#include <iaConsole.h>
+#include <iaString.h>
+using namespace IgorAux;
+
+LSystems::LSystems()
+{
+    init();
+}
+
+LSystems::~LSystems()
+{
+    deinit();
+}
+
+void LSystems::init()
+{
+    con(" -- 3D Example --" << endl);
+
+    // setup window
+    _window.setTitle("Igor - LSystems");
+    _window.setSize(1280, 758);
+    _window.setCentered();
+    _window.registerWindowCloseDelegate(WindowCloseDelegate(this, &LSystems::onWindowClosed));
+    _window.registerWindowResizeDelegate(WindowResizeDelegate(this, &LSystems::onWindowResized));
+
+    // setup perspective view
+    _view.setClearColor(iaColor4f(0.25f, 0.25f, 0.25f, 1));
+    _view.setPerspective(45);
+    _view.setClipPlanes(0.1f, 10000.f);
+    _window.addView(&_view);
+
+    // setup orthogonal view
+    _viewOrtho.setClearColor(false);
+    _viewOrtho.setClearDepth(false);
+    _viewOrtho.setOrthogonal(0, _window.getClientWidth(), _window.getClientHeight(), 0);
+    _viewOrtho.registerRenderDelegate(RenderDelegate(this, &LSystems::onRenderOrtho));
+    _window.addView(&_viewOrtho);
+
+    _window.open();
+
+    // init scene
+    _scene = iSceneFactory::getInstance().createScene();
+    // bind scene to perspective view
+    _view.setScene(_scene);
+
+    // setup camera
+    // we want a camera which can be rotated arround the origin
+    // we will acchive that with 3 transform nodes
+    // one is for the heading
+    iNodeTransform* cameraHeading = static_cast<iNodeTransform*>(iNodeFactory::getInstance().createNode(iNodeType::iNodeTransform));
+    // give the transform node a name. naming is optional and ist jus for helping to debug. 
+    // Names do not have to be unique but since it is possible to find nodes by name they better are
+    cameraHeading->setName("camera heading");
+    cameraHeading->rotate(0.1, iaAxis::Y);
+    cameraHeading->rotate(-0.5, iaAxis::X);
+    _cameraHeading = cameraHeading->getID();
+    // one is for the pitch
+    iNodeTransform* cameraPitch = static_cast<iNodeTransform*>(iNodeFactory::getInstance().createNode(iNodeType::iNodeTransform));
+    cameraPitch->setName("camera pitch");
+    _cameraPitch = cameraPitch->getID();
+    // and one is for translation or distance from the origin
+    iNodeTransform* cameraTranslation = static_cast<iNodeTransform*>(iNodeFactory::getInstance().createNode(iNodeType::iNodeTransform));
+    cameraTranslation->setName("camera translation");
+    // translate away from origin
+    cameraTranslation->translate(0, 0, 100);
+    _cameraTranslation = cameraTranslation->getID();
+    // from all nodes that we want to control later we save the node ID
+    // and last but not least we create a camera node
+    iNodeCamera* camera = static_cast<iNodeCamera*>(iNodeFactory::getInstance().createNode(iNodeType::iNodeCamera));
+    camera->setName("camera");
+    // and build everything together
+    // first we add the heading to the root node
+    _scene->getRoot()->insertNode(cameraHeading);
+    // than the pitch to the heading node
+    cameraHeading->insertNode(cameraPitch);
+    // then the translation to the pitch node
+    cameraPitch->insertNode(cameraTranslation);
+    // and than we add the camera to the translation
+    cameraTranslation->insertNode(camera);
+    // and finally we set the camera active. for this to work a camera must be part of a scene 
+    // wich we achived by adding all those nodes on to an other starting with the root node
+    camera->makeCurrent();
+
+    // TODO
+
+    // init render statistics
+    _font = new iTextureFont("StandardFont.png");
+    iStatistics::getInstance().setVerbosity(iRenderStatisticsVerbosity::FPSAndMetrics);
+
+    _taskFlushTexturesID = iTaskManager::getInstance().addTask(new iTaskFlushTextures(&_window));
+
+    // register some callbacks
+    iKeyboard::getInstance().registerKeyUpDelegate(iKeyUpDelegate(this, &LSystems::onKeyPressed));
+    iMouse::getInstance().registerMouseMoveFullDelegate(iMouseMoveFullDelegate(this, &LSystems::onMouseMoved));
+    iMouse::getInstance().registerMouseWheelDelegate(iMouseWheelDelegate(this, &LSystems::onMouseWheel));
+}
+
+void LSystems::deinit()
+{
+    // unregister some callbacks
+    iKeyboard::getInstance().unregisterKeyUpDelegate(iKeyUpDelegate(this, &LSystems::onKeyPressed));
+    iMouse::getInstance().unregisterMouseMoveFullDelegate(iMouseMoveFullDelegate(this, &LSystems::onMouseMoved));
+    iMouse::getInstance().unregisterMouseWheelDelegate(iMouseWheelDelegate(this, &LSystems::onMouseWheel));
+    _window.unregisterWindowCloseDelegate(WindowCloseDelegate(this, &LSystems::onWindowClosed));
+    _window.unregisterWindowResizeDelegate(WindowResizeDelegate(this, &LSystems::onWindowResized));
+    _viewOrtho.unregisterRenderDelegate(RenderDelegate(this, &LSystems::onRenderOrtho));
+
+    // deinit statistics
+    if (_font != nullptr)
+    {
+        delete _font;
+        _font = nullptr;
+    }
+
+    iTaskManager::getInstance().getTask(_taskFlushTexturesID)->abort();
+
+    iSceneFactory::getInstance().destroyScene(_scene);
+    _scene = nullptr;
+
+    if (_window.isOpen())
+    {
+        _window.close();
+        _window.removeView(&_view);
+        _window.removeView(&_viewOrtho);
+    }
+
+    if (_font != nullptr)
+    {
+        delete _font;
+        _font = nullptr;
+    }
+}
+
+void LSystems::onMouseWheel(int32 d)
+{
+    iNodeTransform* camTranslation = static_cast<iNodeTransform*>(iNodeFactory::getInstance().getNode(_cameraTranslation));
+    if (camTranslation != nullptr)
+    {
+        if (d < 0)
+        {
+            camTranslation->translate(0, 0, 10);
+        }
+        else
+        {
+            camTranslation->translate(0, 0, -10);
+        }
+    }
+}
+
+void LSystems::onMouseMoved(int32 x1, int32 y1, int32 x2, int32 y2, iWindow* _window)
+{
+    if (iMouse::getInstance().getLeftButton())
+    {
+        iNodeTransform* cameraPitch = static_cast<iNodeTransform*>(iNodeFactory::getInstance().getNode(_cameraPitch));
+        iNodeTransform* cameraHeading = static_cast<iNodeTransform*>(iNodeFactory::getInstance().getNode(_cameraHeading));
+
+        if (cameraPitch != nullptr &&
+            cameraHeading != nullptr)
+        {
+            cameraPitch->rotate((y2 - y1) * 0.005f, iaAxis::X);
+            cameraHeading->rotate((x1 - x2) * 0.005f, iaAxis::Y);
+
+            iMouse::getInstance().setCenter(true);
+        }
+    }
+}
+
+void LSystems::onWindowClosed()
+{
+    iApplication::getInstance().stop();
+}
+
+void LSystems::onWindowResized(int32 clientWidth, int32 clientHeight)
+{
+    _viewOrtho.setOrthogonal(0, clientWidth, clientHeight, 0);
+}
+
+void LSystems::onKeyPressed(iKeyCode key)
+{
+    if (key == iKeyCode::ESC)
+    {
+        iApplication::getInstance().stop();
+    }
+
+    if (key == iKeyCode::F1)
+    {
+        iNodeVisitorPrintTree printTree;
+        if (_scene != nullptr)
+        {
+            printTree.printToConsole(_scene->getRoot());
+        }
+    }
+}
+
+void LSystems::onRenderOrtho()
+{
+    if (_font != nullptr)
+    {
+        iStatistics::getInstance().drawStatistics(&_window, _font, iaColor4f(1, 1, 1, 1));
+    }
+}
+
+void LSystems::run()
+{
+    iApplication::getInstance().run();
+}
---
+++ examples/10 L-System/src/LSystems.h
@@ -0,0 +1,165 @@
+//
+//   ______                                |\___/|  /\___/\
+//  /\__  _\                               )     (  )     (
+//  \/_/\ \/       __      ___    _ __    =\     /==\     /=
+//     \ \ \     /'_ `\   / __`\ /\`'__\    )   (    )   (
+//      \_\ \__ /\ \L\ \ /\ \L\ \\ \ \/    /     \   /   \
+//      /\_____\\ \____ \\ \____/ \ \_\   |       | /     \
+//  ____\/_____/_\/___L\ \\/___/___\/_/____\__  _/__\__ __/________________
+//                 /\____/                   ( (       ))
+//                 \_/__/                     ) )     ((
+//                                           (_(       \)
+//    (c) Copyright 2014-2015 by Martin Loga
+//
+// This library is free software; you can redistribute it and or modify it   
+// under the terms of the GNU Lesser General Public License as published by  
+// the Free Software Foundation; either version 3 of the License, or (at   
+// your option) any later version.                                           
+// 
+// This library is distributed in the hope that it will be useful,           
+// but WITHOUT ANY WARRANTY; without even the implied warranty of            
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         
+// Lesser General Public License for more details.                           
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.If not, see <http://www.gnu.org/licenses/>.
+// 
+// contact: martinloga@gmx.de  
+
+#ifndef __LSYSTEMS__
+#define __LSYSTEMS__
+
+#include <Igor.h>
+#include <iWindow.h>
+#include <iView.h>
+#include <iTimerHandle.h>
+#include <iModelResourceFactory.h>
+#include <iKeyCodeMap.h>
+using namespace Igor;
+
+#include <iaMatrix.h>
+using namespace IgorAux;
+
+namespace Igor
+{
+	class iScene;
+	class iNodeTransform;
+    class iNodeLight;
+    class iNodeSwitch;
+    class iTextureFont;
+    class iTaskFlushModels;
+    class iTaskFlushTextures;
+    class iNodeLODTrigger;
+    class iNodeLODSwitch;
+    class iNodeModel;
+}
+
+/*! the LSystems example class
+*/
+class LSystems
+{
+
+public:
+
+    /*! init
+    */
+    LSystems();
+
+    /*! deinit
+    */
+    virtual ~LSystems();
+
+    /*! run example
+    */
+    void run();
+
+private:
+
+    /*! the window
+    */
+	iWindow _window;
+
+    /*! the view we render 3D to
+    */
+	iView _view;
+
+    /*! the view we render 2D to
+    */
+    iView _viewOrtho;
+
+    /*! the scene holding our 3d nodes
+    */
+    iScene* _scene = nullptr;
+
+    /*! texture fon we use to render statistics
+    */
+    iTextureFont* _font = nullptr;
+
+    /*! id to transform node used for manipulating the heading of the camera
+    */
+    uint32 _cameraHeading = iNode::INVALID_NODE_ID;
+
+    /*! id to transform node used for manipulating the pitch of the camera
+    */
+    uint32 _cameraPitch = iNode::INVALID_NODE_ID;
+
+    /*! id to transform node used for manipulating the distance of the camera to the origin
+    */
+    uint32 _cameraTranslation = iNode::INVALID_NODE_ID;
+
+    /*! timer handle to control the movement of the light source over time
+    */
+    iTimerHandle* _animationTimingHandle = nullptr;
+
+    /*! id of task to load textures
+    */
+    uint64 _taskFlushTexturesID = iTask::INVALID_TASK_ID;
+
+    /*! called on key pressed event
+
+    \param key the key code of the pressed key
+    */
+	void onKeyPressed(iKeyCode key);
+
+    /*! called when window was closed
+    */
+	void onWindowClosed();
+
+    /*! called when window was resized
+
+    \param clientWidth the client rectangle width
+    \param clientHeight the client rectangle height
+    */
+    void onWindowResized(int32 clientWidth, int32 clientHeight);
+
+    /*! called when the mouse was moved
+
+    \param x1 last horrizontal position
+    \param y1 last vertical position
+    \param x2 current horrizontal position
+    \param y2 current vertical position
+    \param window corresponding window
+    */
+    void onMouseMoved(int32 x1, int32 y1, int32 x2, int32 y2, iWindow* window);
+
+    /*! called when mouse wheel was turned
+
+    \param d mouse wheel delta
+    */
+    void onMouseWheel(int32 d);
+
+    /*! called by orthogonal view
+    */
+    void onRenderOrtho();
+
+    /*! deinit example
+    */
+	void deinit();
+
+    /*! init example
+    */
+	void init();
+
+};
+
+#endif
