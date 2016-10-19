3384eb1dc08373ee23446f482ef68480f2241694
tanzfisch <martinloga@protonmail.com>
2016-10-19 14:24:11 +0200

    minor

--- src/Igor/src/Igor.cpp
+++ src/Igor/src/Igor.cpp
@@ -21,7 +21,7 @@ using namespace IgorAux;
 #include <iWidgetManager.h>
 #include <iStatistics.h>
 #ifdef __USE_OPENAL__
-    #include <iSoundResourceFactory.h>
+#include <iSoundResourceFactory.h>
 #endif
 #include <iTimer.h>
 #include <iRenderer.h>
@@ -32,21 +32,21 @@ using namespace IgorAux;
 #include <iPhysics.h>
 
 #ifdef __IGOR_MSCOMPILER__
-	#pragma comment(lib, "OpenGL32.lib")
-	#pragma comment(lib, "glu32.lib")
+#pragma comment(lib, "OpenGL32.lib")
+#pragma comment(lib, "glu32.lib")
 #ifdef __USE_OPENAL__
-	#pragma comment(lib, "alut.lib")
-	#pragma comment(lib, "OpenAL32.lib")
+#pragma comment(lib, "alut.lib")
+#pragma comment(lib, "OpenAL32.lib")
 #endif
 
 #ifdef __IGOR_DEBUG__
-	#pragma comment(lib, "IgorAux_d.lib")
-    #pragma comment(lib, "OMPF_d.lib")
-    #pragma comment(lib, "newton_d.lib")
+#pragma comment(lib, "IgorAux_d.lib")
+#pragma comment(lib, "OMPF_d.lib")
+#pragma comment(lib, "newton_d.lib")
 #else
-	#pragma comment(lib, "IgorAux.lib")
-    #pragma comment(lib, "OMPF.lib")
-    #pragma comment(lib, "newton.lib")
+#pragma comment(lib, "IgorAux.lib")
+#pragma comment(lib, "OMPF.lib")
+#pragma comment(lib, "newton.lib")
 #endif
 #endif
 
@@ -56,22 +56,22 @@ using namespace IgorAux;
 //! gets called when the dll is loaded into memory
 bool WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
-	switch(fdwReason)
-	{
-	case DLL_PROCESS_ATTACH: //!< when loaded
-		break;
-	case DLL_THREAD_ATTACH:
-		break;
-	case DLL_THREAD_DETACH:
-		break;
-	case DLL_PROCESS_DETACH: //!< when unloaded
-		break;
+    switch (fdwReason)
+    {
+    case DLL_PROCESS_ATTACH: //!< when loaded
+        break;
+    case DLL_THREAD_ATTACH:
+        break;
+    case DLL_THREAD_DETACH:
+        break;
+    case DLL_PROCESS_DETACH: //!< when unloaded
+        break;
 
-	default:
-		break;
-	}
+    default:
+        break;
+    }
 
-	return true;
+    return true;
 }
 #endif
 
@@ -79,24 +79,41 @@ namespace Igor
 {
 
     void printInfo()
-	{
+    {
 #ifdef __IGOR_DEBUG__ 
-    #define G iaForegroundColor::Gray
-    #define W iaForegroundColor::DarkBlue
-    #define C1 iaForegroundColor::Gray
-    #define C2 iaForegroundColor::Gray
-    #define M iaForegroundColor::Gray
-    #define T iaForegroundColor::Gray
+#define G iaForegroundColor::Gray
+#define W iaForegroundColor::DarkBlue
+#define C1 iaForegroundColor::Gray
+#define C2 iaForegroundColor::Gray
+#define M iaForegroundColor::Gray
+#define T iaForegroundColor::Gray
 #else
-    #define G iaForegroundColor::Green
-    #define W iaForegroundColor::DarkCyan
-    #define C1 iaForegroundColor::Green
-    #define C2 iaForegroundColor::Green
-    #define M iaForegroundColor::Green
-    #define T iaForegroundColor::DarkMagenta
+#define G iaForegroundColor::Green
+#define W iaForegroundColor::DarkCyan
+#define C1 iaForegroundColor::Green
+#define C2 iaForegroundColor::Green
+#define M iaForegroundColor::Green
+#define T iaForegroundColor::DarkMagenta
+#endif
+#define R iaForegroundColor::Red
+
+        // source of text http://patorjk.com/software/taag/#p=display&f=Larry%203D&t=powered%20by%20%0AIgor
+
+        iaConsole::getInstance() << LOCK << endl;
+
+#if 0
+        iaConsole::getInstance() << R << "                                                    __      __                       " << endl;
+        iaConsole::getInstance() << R << "                                                   /\\ \\    /\\ \\                      " << endl;
+        iaConsole::getInstance() << R << "  _____     ___   __  __  __     __   _ __    __   \\_\\ \\   \\ \\ \\____  __  __         " << endl;
+        iaConsole::getInstance() << R << " /\\ '__`\\  / __`\\/\\ \\/\\ \\/\\ \\  /'__`\\/\\`'__\\/'__`\\ /'_` \\   \\ \\ '__`\\/\\ \\/\\ \\        " << endl;
+        iaConsole::getInstance() << R << " \\ \\ \\L\\ \\/\\ \\L\\ \\ \\ \\_/ \\_/ \\/\\  __/\\ \\ \\//\\  __//\\ \\L\\ \\   \\ \\ \\L\\ \\ \\ \\_\\ \\       " << endl;
+        iaConsole::getInstance() << R << "  \\ \\ ,__/\\ \\____/\\ \\___x___/'\\ \\____\\\\ \\_\\\\ \\____\\ \\___,_\\   \\ \\_,__/\\/`____ \\      " << endl;
+        iaConsole::getInstance() << R << "   \\ \\ \\/  \\/___/  \\/__//__/   \\/____/ \\/_/ \\/____/\\/__,_ /    \\/___/  `/___/> \\     " << endl;
+        iaConsole::getInstance() << R << "    \\ \\_\\                                                                 /\\___/     " << endl;
+        iaConsole::getInstance() << R << "     \\/_/                                                                 \\/__/      " << endl;
+        iaConsole::getInstance() << G << "" << endl;
 #endif
 
-		iaConsole::getInstance() << LOCK << endl;
         iaConsole::getInstance() << G << "   ______                               " << C1 << " |\\___/|" << C2 << "  /\\___/\\            " << endl;
         iaConsole::getInstance() << G << "  /\\__  _\\                              " << C1 << " )     ( " << C2 << " )     (" << endl;
         iaConsole::getInstance() << G << "  \\/_/\\ \\/       __      ___    _ __    " << C1 << "=\\     /=" << C2 << "=\\     /=" << endl;
@@ -107,13 +124,13 @@ namespace Igor
         iaConsole::getInstance() << G << "                 /\\____/                " << C1 << "   ( (   " << C2 << "    ))               " << M << "(_" << endl;
         iaConsole::getInstance() << G << "                 \\_/__/  game engine     " << C1 << "   ) )  " << C2 << "   ((" << endl;
         iaConsole::getInstance() << G << "                                        " << C1 << "   (_(    " << C2 << "   \\)" << endl;
-		
+
         iaConsole::getInstance() << T << "    (c) Copyright 2014-2016 by Martin Loga" << endl << endl;
-        iaConsole::getInstance() << T << "    ver. " << __IGOR_VERSION__ << " (" << __IGOR_CONFIGURATION__ << ") LGPL v3.0"<< endl << endl;
+        iaConsole::getInstance() << T << "    ver. " << __IGOR_VERSION__ << " (" << __IGOR_CONFIGURATION__ << ") LGPL v3.0" << endl << endl;
         iaConsole::getInstance() << T << "    thanks to M. Rochel, M. Schulz, T. Drevensek, I. Yozova" << endl << endl;
         iaConsole::getInstance() << T << "    powered by NewtonDynamics, OpenGL, GLee, stb_image, TinyXML" << endl;
-		iaConsole::getInstance() << W << "  ____________________________________________________________________________" << endl << endl;
-		iaConsole::getInstance() << UNLOCK;
+        iaConsole::getInstance() << W << "  ____________________________________________________________________________" << endl << endl;
+        iaConsole::getInstance() << UNLOCK;
 
 #undef G
 #undef W
@@ -123,17 +140,17 @@ namespace Igor
 #undef T
 
         IgorAux::printInfo();
-	}
+    }
 
-	void startup()
-	{
-		startup(0, nullptr);
-	}
+    void startup()
+    {
+        startup(0, nullptr);
+    }
 
-	void startup(int argc, wchar_t** argv)
-	{
+    void startup(int argc, wchar_t** argv)
+    {
         IgorAux::startup();
-		printInfo();
+        printInfo();
 
         iConfigReader configReader;
 
@@ -161,15 +178,15 @@ namespace Igor
                 }
             }
         }
-		else
-		{
+        else
+        {
             iaFile file(L"..\\config\\Igor.xml");
-			if(file.exist())
-			{
-				configReader.readConfiguration(file.getFullFileName());
-			}
-			else
-			{
+            if (file.exist())
+            {
+                configReader.readConfiguration(file.getFullFileName());
+            }
+            else
+            {
                 iaDirectory::setCurrentDirectory(iaDirectory::getApplicationDirectory());
                 iaFile file(L"..\\config\\Igor.xml");
                 if (file.exist())
@@ -180,14 +197,14 @@ namespace Igor
                 {
                     con_warn("default config file " << file.getFileName() << " not found");
                 }
-			}
-		}
-	}
+            }
+        }
+    }
 
-	void shutdown()
-	{
+    void shutdown()
+    {
         //! \todo somehow we have to figure out dependencies and shutdown in correct order!
-		// don't change the shutdown order
+        // don't change the shutdown order
         if (iModelResourceFactory::isInstantiated())
         {
             iModelResourceFactory::destroyInstance();
@@ -198,20 +215,20 @@ namespace Igor
             iStatistics::destroyInstance();
         }
 
-        if(iTaskManager::isInstantiated())
-		{
-			iTaskManager::destroyInstance();
-		}
+        if (iTaskManager::isInstantiated())
+        {
+            iTaskManager::destroyInstance();
+        }
 
-		if(iTimer::isInstantiated())
-		{
-			iTimer::destroyInstance();
-		}
+        if (iTimer::isInstantiated())
+        {
+            iTimer::destroyInstance();
+        }
 
-		if (iNodeFactory::isInstantiated())
-		{
-			iNodeFactory::destroyInstance();
-		}
+        if (iNodeFactory::isInstantiated())
+        {
+            iNodeFactory::destroyInstance();
+        }
 
         if (iWidgetManager::isInstantiated())
         {
@@ -228,42 +245,42 @@ namespace Igor
             iMaterialResourceFactory::destroyInstance();
         }
 
-		if(iTextureResourceFactory::isInstantiated())
-		{
-			iTextureResourceFactory::destroyInstance();
-		}
+        if (iTextureResourceFactory::isInstantiated())
+        {
+            iTextureResourceFactory::destroyInstance();
+        }
 
-		if(iRenderer::isInstantiated())
-		{
-			iRenderer::destroyInstance();
-		}
+        if (iRenderer::isInstantiated())
+        {
+            iRenderer::destroyInstance();
+        }
 
-        if(iPhysics::isInstantiated())
+        if (iPhysics::isInstantiated())
         {
             iPhysics::destroyInstance();
         }
 
 #ifdef __USE_OPENAL__
-		if(iSoundResourceFactory::isInstantiated())
-		{
-			iSoundResourceFactory::destroyInstance();
-		}
+        if (iSoundResourceFactory::isInstantiated())
+        {
+            iSoundResourceFactory::destroyInstance();
+        }
 #endif
 
-		if(iKeyCodeMap::isInstantiated())
-		{
-			iKeyCodeMap::destroyInstance();
-		}
+        if (iKeyCodeMap::isInstantiated())
+        {
+            iKeyCodeMap::destroyInstance();
+        }
 
-		if(iKeyboard::isInstantiated())
-		{
-			iKeyboard::destroyInstance();
-		}
+        if (iKeyboard::isInstantiated())
+        {
+            iKeyboard::destroyInstance();
+        }
 
-		if(iMouse::isInstantiated())
-		{
-			iMouse::destroyInstance();
-		}
+        if (iMouse::isInstantiated())
+        {
+            iMouse::destroyInstance();
+        }
 
         if (iApplication::isInstantiated())
         {
@@ -271,6 +288,6 @@ namespace Igor
         }
 
         IgorAux::shutdown();
-	}
+    }
 
 }
