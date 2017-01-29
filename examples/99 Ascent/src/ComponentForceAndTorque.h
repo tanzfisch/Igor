8a61e20f9bc9bd6b75e19c131f5dc96b507a1eb5
tanzfisch <martinloga@protonmail.com>
2017-01-29 11:30:19 +0100

    ecs

---
+++ examples/99 Ascent/src/ComponentForceAndTorque.h
@@ -0,0 +1,46 @@
+//
+//   ______                                |\___/|  /\___/\
+//  /\__  _\                               )     (  )     (
+//  \/_/\ \/       __      ___    _ __    =\     /==\     /=
+//     \ \ \     /'_ `\   / __`\ /\`'__\    )   (    )   (
+//      \_\ \__ /\ \L\ \ /\ \L\ \\ \ \/    /     \   /   \
+//      /\_____\\ \____ \\ \____/ \ \_\   |       | /     \
+//  ____\/_____/_\/___L\ \\/___/___\/_/____\__  _/__\__ __/________________
+//                 /\____/                   ( (       ))
+//                 \_/__/  game engine        ) )     ((
+//                                           (_(       \)
+// (c) Copyright 2014-2016 by Martin Loga
+//
+// This library is free software; you can redistribute it and or modify it   
+// under the terms of the GNU Lesser General Public License as published by  
+// the Free Software Foundation; either version 3 of the License, or (at   
+// your option) any later version.                                           
+// 
+// This library is distributed in the hope that it will be useful,           
+// but WITHOUT ANY WARRANTY; without even the implied warranty of            
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         
+// Lesser General Public License for more details.                           
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.If not, see <http://www.gnu.org/licenses/>.
+// 
+// contact: martinloga@gmx.de  
+
+#ifndef __iCOMPONENT_FORCEANDTORQUE__
+#define __iCOMPONENT_FORCEANDTORQUE__
+
+#include <iComponent.h>
+using namespace Igor;
+
+#include <iaVector3.h>
+using namespace IgorAux;
+
+struct ForceAndTorque
+{
+    iaVector3d _force;
+    iaVector3d _torque;
+};
+
+typedef iComponent<ForceAndTorque> ComponentForceAndTorque;
+
+#endif
