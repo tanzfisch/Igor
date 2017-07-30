6cb79ab70e9a67f74c04f167f9bdea802200bf03
tanzfisch <martinloga@protonmail.com>
2017-07-30 18:34:38 +1200

    removed some warnings

--- src/Igor/src/iDialogGraph.h
+++ src/Igor/src/iDialogGraph.h
@@ -142,27 +142,27 @@ namespace Igor
 
         /*! min value on x axis
         */
-        float32 _xMin = 0;
+        float32 _xMin = 0.0f;
 
         /*! max value on x axis
         */
-        float32 _xMax = 1;
+        float32 _xMax = 1.0f;
 
         /*! up down stepping of number choosers for x axis values
         */
-        float32 _xStepping = 0.1;
+        float32 _xStepping = 0.1f;
 
         /*! min value on y axis
         */
-        float32 _yMin = 0;
+        float32 _yMin = 0.0f;
 
         /*! max value on y axis
         */
-        float32 _yMax = 1;
+        float32 _yMax = 1.0f;
 
         /*! up down stepping of number choosers for y axis values
         */
-        float32 _yStepping = 0.1;
+        float32 _yStepping = 0.1f;
 
         /*! index of selected value
         */
