22c62b76b33b5077d48afbdf93a6944214928284
tanzfisch <martinloga@protonmail.com>
2019-03-27 23:24:59 +1300

    #80 introduced iRay

---
+++ src/Igor/src/iRay.h
@@ -0,0 +1,85 @@
+//
+//   ______                                |\___/|  /\___/\
+//  /\__  _\                               )     (  )     (
+//  \/_/\ \/       __      ___    _ __    =\     /==\     /=
+//     \ \ \     /'_ `\   / __`\ /\`'__\    )   (    )   (
+//      \_\ \__ /\ \L\ \ /\ \L\ \\ \ \/    /     \   /   \
+//      /\_____\\ \____ \\ \____/ \ \_\   |       | /     \
+//  ____\/_____/_\/___L\ \\/___/___\/_/____\__  _/__\__ __/________________
+//                 /\____/                   ( (       ))
+//                 \_/__/  game engine        ) )     ((
+//                                           (_(       \)
+// (c) Copyright 2012-2018 by Martin Loga
+//
+// This library is free software; you can redistribute it and or modify it   
+// under the terms of the GNU Lesser General Public License as published by  
+// the Free Software Foundation; either version 3 of the License, or (at   
+// your option) any later version.                                           
+// 
+// This library is distributed in the hope that it will be useful,           
+// but WITHOUT ANY WARRANTY; without even the implied warranty of            
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         
+// Lesser General Public License for more details.                           
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.If not, see <http://www.gnu.org/licenses/>.
+// 
+// contact: martinloga@gmx.de  
+
+#ifndef __iRAY__
+#define __iRAY__
+
+#include <iDefines.h>
+
+#include <iaVector3.h>
+using namespace IgorAux;
+
+namespace Igor
+{
+
+    /*! a ray
+    */
+	template <class T>
+	class Igor_API_Template iRay
+	{
+
+	public:
+
+        /*! position of ray
+        */
+		iaVector3<T> m_pos;
+
+        /*! direction of ray
+        */
+        iaVector3<T> m_dir;
+
+        /*! constructor that basically sets member valirables
+
+        \param pos position of ray
+        \param dir direction of ray
+        */
+		iRay(const iaVector3<T>& pos, const iaVector3<T>& dir);
+
+        /*! does notthing
+        */
+        iRay() = default;
+
+        /*! does notthing
+        */
+        virtual ~iRay() = default;
+
+	};
+
+	#include <iRay.inl>
+
+    /*! float32 precision ray
+    */
+	typedef iRay<float32> iRayf;
+
+    /*! float64 precision ray
+    */
+    typedef iRay<float64> iRayd;
+
+}
+
+#endif
