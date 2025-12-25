d85ae905749b152dfdd71f43637336767823c5bf
Martin Loga <martinloga@protonmail.com>
2025-12-26 11:23:15 +1300

    #457 lua take off

--- src/igor/igor/resources/script/iScriptEngine.cpp
+++ src/igor/igor/resources/script/iScriptEngine.cpp
@@ -5,11 +5,13 @@
 #include <igor/resources/script/iScriptEngine.h>
 
 #include <iaux/system/iaConsole.h>
+using namespace iaux;
 
-extern "C" {
-    #include <luajit.h>
-    #include <lauxlib.h>
-    #include <lualib.h>
+extern "C"
+{
+#include <luajit.h>
+#include <lauxlib.h>
+#include <lualib.h>
 }
 
 #include <codecvt>
@@ -22,14 +24,18 @@ namespace igor
         return converter.to_bytes(wstr);
     }
 
-    static void print(const std::string &message)
+    static int print(lua_State *lua)
     {
-        con_endl(message.c_str());
+        const auto message = luaL_checkstring(lua, 1);
+        con_endl(message);
+        return 0;
     }
 
-    static void printInfo(const std::string &message)
+    static int printInfo(lua_State *lua)
     {
-        con_info(message.c_str());
+        const auto message = luaL_checkstring(lua, 1);
+        con_info(message);
+        return 0;
     }
 
     class iScriptEngineImpl
@@ -39,8 +45,19 @@ namespace igor
     public:
         iScriptEngineImpl()
         {
-            _chai.add(chaiscript::fun(&print), "print");
-            _chai.add(chaiscript::fun(&printInfo), "printInfo");
+            _lua = luaL_newstate();
+            con_assert_sticky(_lua != nullptr, "failed to init Lua");
+
+            luaL_openlibs(_lua);
+
+            lua_pushcfunction(_lua, print);
+            lua_setglobal(_lua, "con_endl");
+
+            lua_pushcfunction(_lua, printInfo);
+            lua_setglobal(_lua, "con_info");
+
+            //_chai.add(chaiscript::fun(&print), "print");
+            // _chai.add(chaiscript::fun(&printInfo), "printInfo");
 
             // _chai.add(chaiscript::var(&var), wstringToUtf8(name.getData()));
             // _chai.add(chaiscript::const_var(&var), wstringToUtf8(name.getData()));
@@ -48,46 +65,38 @@ namespace igor
             // _chai.add(chaiscript::fun(method, obj), wstringToUtf8(name.getData()));
         }
 
+        bool execute(const char *script)
+        {
+            if (luaL_dostring(_lua, script))
+            {
+                con_err("script: " << lua_tostring(_lua, -1));
+                lua_pop(_lua, 1);
+                return false;
+            }
+
+            return true;
+        }
+
     private:
-        /*! chaiscript instance
+        /*! lua instance
          */
-        chaiscript::ChaiScript _chai;
+        lua_State *_lua = nullptr;
     };
 
     iScriptEngine::iScriptEngine()
     {
         _impl = std::make_unique<iScriptEngineImpl>();
 
-        _impl->_chai.eval(R"(
-            printInfo("ChaiScript initialized");
-            )");
+        const char *script = R"(con_info("LuaJIT initialized!"))";
+        _impl->execute(script);
     }
 
     iScriptEngine::~iScriptEngine()
     {
     }
 
-    void iScriptEngine::executeScript(const iaString &filename)
+    void iScriptEngine::execute(const iaString &script)
     {
-        try
-        {
-            _impl->_chai.use(wstringToUtf8(filename.getData()));
-        }
-        catch (const chaiscript::exception::eval_error &e)
-        {
-            con_err("load error '" << filename << "': " << e.pretty_print().c_str());
-        }
-    }
-
-    void iScriptEngine::execute(const iaString &code)
-    {
-        try
-        {
-            _impl->_chai.eval(wstringToUtf8(code.getData()));
-        }
-        catch (const chaiscript::exception::eval_error &e)
-        {
-            con_err("eval error: " << e.pretty_print().c_str());
-        }
+        _impl->execute(wstringToUtf8(script.getData()).c_str());
     }
 }
